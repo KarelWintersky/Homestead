117833f36627a18da59e3c3e6fc8ad5d6b1989ea
Karel Wintersky <wombat@fontanka.ru>
2025-04-17 06:22:38 +0300

    0.2.0
    Base version
    + nanoRedis Wrapper
    + new `box.json` (compile both engine and vendor to `homestead.phar`)

--- engine/Homestead.php
+++ engine/Homestead.php
@@ -2,20 +2,41 @@
 
 namespace Homestead;
 
+use Arris\Toolkit\RedisClient;
 use http\Exception\RuntimeException;
 use Symfony\Component\Yaml\Yaml;
 
 class Homestead
 {
-    public static array $configs = [];
+    public static array $config_files = [];
+
+    public static $_config;
+    public static $_layout;
+    public static $_sections_raw;
+    public static $_sections;
+
+    public static RedisClient|null $service_redis = null;
+
     public static function init()
     {
-        self::$configs['layout'] = CONFIG_PATH . '/_layout.yml';
-        self::$configs['sections'] = CONFIG_PATH . '/_sections.yml';
-
+        self::$config_files['config'] = CONFIG_PATH . '/_config.yml';
+        self::$config_files['layout'] = CONFIG_PATH . '/_layout.yml';
+        self::$config_files['sections'] = CONFIG_PATH . '/_sections.yml';
     }
 
-    public static function loadYamlConfig($filename): mixed
+    public static function loadCredentials():mixed
+    {
+        $source = self::$config_files['config'];
+        if (!file_exists($source)) {
+            throw new \RuntimeException("Missing config/_config.yml file");
+        }
+
+        self::$_config = $systemConfig = self::loadYaml($source);
+
+        return $systemConfig;
+    }
+
+    public static function loadYaml($filename): mixed
     {
         return Yaml::parseFile($filename);
     }
@@ -25,25 +46,30 @@ class Homestead
      */
     public static function loadLayoutConfig(): mixed
     {
-        if (!file_exists(CONFIG_PATH . '/_layout.yml')) {
+        $source = self::$config_files['layout'];
+        if (!file_exists($source)) {
             throw new \RuntimeException("Missing config/_layout.yml file");
         }
 
-        $layoutConfig = self::loadYamlConfig(CONFIG_PATH . '/_layout.yml');
+        self::$_layout = $layoutConfig = self::loadYaml($source);
 
         return $layoutConfig;
     }
 
     public static function loadAllSectionsConfig(): mixed
     {
-        if (!file_exists(CONFIG_PATH . '/_sections.yml')) {
+        $source = self::$config_files['sections'];
+
+        if (!file_exists($source)) {
             throw new \RuntimeException("Missing config/_sections.yml file");
         }
-        $allSectionsConfig = self::loadYamlConfig(CONFIG_PATH . '/_sections.yml');
+        $allSectionsConfig = self::loadYaml($source);
 
         if (!isset($allSectionsConfig['sections'])) {
             throw new RuntimeException("Missing 'sections' in config/_sections.yml file");
         }
+
+        self::$_sections_raw = $allSectionsConfig;
 
         return $allSectionsConfig;
     }
@@ -53,7 +79,7 @@ class Homestead
         $sections = [];
 
         foreach ($allSectionsConfig['sections'] as $sectionFile) {
-            $sectionConfig = self::loadYamlConfig(CONFIG_PATH . DIRECTORY_SEPARATOR . $sectionFile);
+            $sectionConfig = self::loadYaml(CONFIG_PATH . DIRECTORY_SEPARATOR . $sectionFile);
 
             if ($sectionConfig && isset($sectionConfig['resources'])) {
                 $sections[] = [
@@ -63,6 +89,8 @@ class Homestead
                 ];
             }
         }
+
+        self::$_sections = $sections;
 
         return $sections;
     }
--- engine/bootstrap.php
+++ engine/bootstrap.php
@@ -3,3 +3,13 @@
 use Homestead\Homestead;
 
 Homestead::init();
+$credentials = Homestead::loadCredentials();
+
+$redis = new \Arris\Toolkit\RedisClient();
+
+if (isset($credentials['redis'])) {
+    $cr = $credentials['redis'];
+    $redis->connect($cr['host'], $cr['port'], $cr['database'], $cr['enable']);
+}
+
+Homestead::$service_redis = $redis;
